8f57a1b4741c5f7145fc8d892b8ff2b6ea530c71
zcluo <zhcluo@gmail.com>
2018-11-15 15:03:09 +0800

    去除ss安装
    去除ss安装

--- shell/all_install.sh
+++ shell/all_install.sh
@@ -24,11 +24,11 @@ apt install -y expect
 wget --no-check-certificate -O install_bbr_expect.sh https://raw.githubusercontent.com/zcluo/vps/master/shell/install_bbr_expect.sh
 chmod +x install_bbr_expect.sh
 ./install_bbr_expect.sh
-wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
-chmod +x shadowsocks-all.sh
-wget --no-check-certificate -O install_ss_expect.sh https://raw.githubusercontent.com/zcluo/vps/master/shell/install_ss_expect.sh
-chmod +x install_ss_expect.sh
-./install_ss_expect.sh
+#wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
+#chmod +x shadowsocks-all.sh
+#wget --no-check-certificate -O install_ss_expect.sh https://raw.githubusercontent.com/zcluo/vps/master/shell/install_ss_expect.sh
+#chmod +x install_ss_expect.sh
+#./install_ss_expect.sh
 mkdir -p /usr/local/caddy/www/file
 cd /usr/local/caddy/www/file
 dd if=/dev/urandom of=test bs=100M count=1 iflag=fullblock
