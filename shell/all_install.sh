8f02bc34044314b58e20390407a13fce7e2c6da0
zcluo <zhcluo@gmail.com>
2019-12-02 11:25:22 +0800

    去除版本号限定，去除trojan和snell安装
    去除版本号限定，去除trojan和snell安装

--- shell/all_install.sh
+++ shell/all_install.sh
@@ -4,7 +4,7 @@ apt install curl screen net-tools iperf3 -y
 wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/caddy_install.sh && chmod +x caddy_install.sh && bash caddy_install.sh install
 wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/across/master/bbr.sh
 mkdir -p /usr/local/caddy/
-bash <(curl -L -s https://install.direct/go.sh) --version v4.18.0
+bash <(curl -L -s https://install.direct/go.sh) 
 sleep 20
 wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/Caddyfile -O /usr/local/caddy/Caddyfile
 wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/config.json -O /etc/v2ray/config.json
@@ -53,28 +53,28 @@ chmod +x /etc/rc.local
 systemctl start rc-local
 
 #增加trojan安装
-apt update && apt -y install git build-essential cmake libboost-system-dev libboost-program-options-dev libssl-dev default-libmysqlclient-dev
-cd
-git clone https://github.com/trojan-gfw/trojan.git
-cd trojan/
-mkdir build
-cd build/
-cmake ..
-make
-ctest
-make install
+#apt update && apt -y install git build-essential cmake libboost-system-dev libboost-program-options-dev libssl-dev default-libmysqlclient-dev
+#cd
+#git clone https://github.com/trojan-gfw/trojan.git
+#cd trojan/
+#mkdir build
+#cd build/
+#cmake ..
+#make
+#ctest
+#make install
 
-mkdir -p /etc/trojan/
-wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/trojan.cfg -O /etc/trojan/trojan.cfg
-sed -e "s/xxx\.xxxxxx\.xxx/$1/g" /etc/trojan/trojan.cfg > /etc/trojan/trojan.cfg.new
-\mv /etc/trojan/trojan.cfg.new /etc/trojan/trojan.cfg
+#mkdir -p /etc/trojan/
+#wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/trojan.cfg -O /etc/trojan/trojan.cfg
+#sed -e "s/xxx\.xxxxxx\.xxx/$1/g" /etc/trojan/trojan.cfg > /etc/trojan/trojan.cfg.new
+#\mv /etc/trojan/trojan.cfg.new /etc/trojan/trojan.cfg
 
 
-sed -i '/exit 0/i nohup trojan -c /etc/trojan/trojan.cfg -l /var/log/trojan.log > trojan.out 2>&1 &' /etc/rc.local
+#sed -i '/exit 0/i nohup trojan -c /etc/trojan/trojan.cfg -l /var/log/trojan.log > trojan.out 2>&1 &' /etc/rc.local
 
-cd
-wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/snell.sh
-chmod +x snell.sh
-./snell.sh
+#cd
+#wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/snell.sh
+#chmod +x snell.sh
+#./snell.sh
 
 reboot
