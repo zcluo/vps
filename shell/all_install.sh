9639a07a99a4c6560b16e321dbf5fbd34e45bfcf
zcluo <zhcluo@gmail.com>
2020-03-07 17:05:58 +0800

    bbr.sh source update
    bbr.sh source update

--- shell/all_install.sh
+++ shell/all_install.sh
@@ -2,7 +2,8 @@
 echo "$1"
 apt install curl screen net-tools iperf3 -y
 wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/caddy_install.sh && chmod +x caddy_install.sh && bash caddy_install.sh install
-wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/across/master/bbr.sh
+#wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/across/master/bbr.sh
+wget -N --no-check-certificate https://raw.githubusercontent.com/teddysun/across/master/bbr.sh
 mkdir -p /usr/local/caddy/
 bash <(curl -L -s https://install.direct/go.sh) 
 sleep 20
