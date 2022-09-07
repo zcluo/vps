ed5e143aa4854366eb668ecae46894c89e8277a7
zcluo <zhcluo@gmail.com>
2022-09-07 21:27:50 +0800

    Update v2rayud.sh

--- shell/v2rayud.sh
+++ shell/v2rayud.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 bash <(curl -L https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh)
+systemctl stop v2ray
+systemctl disable v2ray
+systemctl restart rc-local
