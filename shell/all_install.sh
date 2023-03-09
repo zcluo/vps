22936e9ff1033ce29ece56ecc83bb611ef2a4d7c
zcluo <zhcluo@gmail.com>
2023-03-09 13:07:38 +0800

    Update all_install.sh

--- shell/all_install.sh
+++ shell/all_install.sh
@@ -112,7 +112,7 @@ chmod +x /usr/local/bin/oh-my-posh
 
 mkdir ~/.poshthemes
 wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
-unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
+unzip ~/.poshthemes/themes.zip -d -o ~/.poshthemes
 chmod u+rw ~/.poshthemes/*.omp.*
 rm ~/.poshthemes/themes.zip
 echo 'eval "$(oh-my-posh --init --shell bash --config /root/.poshthemes/1_shell.omp.json)"' >> .bashrc
