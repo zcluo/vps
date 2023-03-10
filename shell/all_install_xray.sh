d68e555af3dc98fcf4bac4fc44732b9820581420
zcluo <zhcluo@gmail.com>
2023-03-10 12:53:00 +0800

    Update all_install_xray.sh

--- shell/all_install_xray.sh
+++ shell/all_install_xray.sh
@@ -5,76 +5,72 @@ echo " e.g.: $0 domain_name username password emailaddress uuid"
 exit 1;
 fi
 
-systemctl stop v2ray
-systemctl disable  v2ray
-systemctl stop xray
-systemctl disable  xray
-\rm -f /usr/local/etc/xray/*
+systemctl stop xray && systemctl disable xray
+systemctl stop v2ray && systemctl disable v2ray
+
 #used for uuid replacement
 uuid=$(cat /proc/sys/kernel/random/uuid)
 echo "$1" "$2" "$3" "$4" "$5"
-apt install curl screen net-tools iperf3 ca-certificates git lsof apt-transport-https ca-certificates  -y
+apt install curl screen net-tools iperf3 ca-certificates git lsof apt-transport-https ca-certificates neofetch unzip certbot nginx  -y
 #wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/caddy_install.sh && chmod +x caddy_install.sh && bash caddy_install.sh install
 #wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/across/master/bbr.sh
->/etc/apt/sources.list.d/caddy-fury.list
-echo "deb [trusted=yes] https://apt.fury.io/caddy/ /" |  tee -a /etc/apt/sources.list.d/caddy-fury.list
-apt update
-apt install caddy
+systemctl stop nginx
 #\rm -f /etc/apt/sources.list.d/caddy-fury.list
 \chmod -R 777  /var/log/
 wget -N --no-check-certificate https://raw.githubusercontent.com/teddysun/across/master/bbr.sh
+#wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/install_ohmyposh.sh
 #mkdir -p /usr/local/caddy/
 \mkdir -p /var/log/xray/
+\mkdir -p /var/log/nginx
 \chmod -R 777  /var/log/xray/
-# curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
-# chmod +x install-release.sh
-# bash install-release.sh
-# bash <(curl -L https://raw.githubusercontent.com/XTLS/Xray-install/main/install-release.sh)
-curl -O https://raw.githubusercontent.com/XTLS/Xray-install/main/install-release.sh
-chmod +x install-release.sh
-bash install-release.sh --beta
+\chmod -R 777 /var/log/nginx
+bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install  --beta
+#curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
+#chmod +x install-release.sh
+#bash install-release.sh
 sleep 20
 
 #\mkdir -p /etc/caddy/
-wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/Caddyfile -O /etc/caddy/Caddyfile
-wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/config_xray.json -O /usr/local/etc/xray/config_xray.json
-sed -e "s/xxx\.xxxxxx\.xxx/$1/g" /etc/caddy/Caddyfile > /etc/caddy/Caddyfile.new
-sed -e "s/user/$2/g" /etc/caddy/Caddyfile.new > /etc/caddy/Caddyfile
-sed -e "s/pass/$3/g" /etc/caddy/Caddyfile > /etc/caddy/Caddyfile.new
-sed -e "s/xxx\@xxx\.xxx/$4/g" /etc/caddy/Caddyfile.new > /etc/caddy/Caddyfile
-#wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/caddy.service -O /lib/systemd/system/caddy.service
-sed -e "s/xxx\.xxxxxx\.xxx/$1/g" /usr/local/etc/xray/config_xray.json > /usr/local/etc/xray/config_xray.json.new
-sed -e "s/trojanpass/$3/g" /usr/local/etc/xray/config_xray.json.new > /usr/local/etc/xray/config_xray.json
-sed -e "s/xxx\@xxx\.xxx/$4/g"   /usr/local/etc/xray/config_xray.json > /usr/local/etc/xray/config_xray.json.new
-sed -e "s/xxxxxxxx\-xxxx\-xxxx\-xxxx\-xxxxxxxxxxxx/$5/g"   /usr/local/etc/xray/config_xray.json.new > /usr/local/etc/xray/config.json
-#\mv /etc/caddy/Caddyfile.new  /etc/caddy/Caddyfile
-#\mv /usr/local/etc/xray/config.json.new /usr/local/etc/xray/config.json
-chmod -x /etc/systemd/system/xray.service
-systemctl enable caddy && systemctl restart caddy 
-sleep 20
-cd /var/lib/caddy
-chmod -R 755 .local/
-systemctl enable xray && systemctl restart xray
+\rm -rf /usr/local/etc/xray/*
 
+
+wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/config_xray.json -O /usr/local/etc/xray/config.json
+wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/nginx-xray-template.conf -O /etc/nginx/nginx.conf
+
+
+
+certbot certonly --standalone -d  $1 -m $4 --agree-tos -n
+sed -i "s/xxx\.xxxxxx\.xxx/$1/g" /etc/nginx/nginx.conf
+
+#wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/caddy.service -O /lib/systemd/system/caddy.service
+sed -i "s/xxx\.xxxxxx\.xxx/$1/g" /usr/local/etc/xray/config.json
+sed -i "s/trojanpass/$3/g" /usr/local/etc/xray/config.json
+sed -i "s/xxx\@xxx\.xxx/$4/g"   /usr/local/etc/xray/config.json
+sed -i "s/xxxxxxxx\-xxxx\-xxxx\-xxxx\-xxxxxxxxxxxx/$5/g"   /usr/local/etc/xray/config.json
+
+chmod -x /etc/systemd/system/xray.service
+
+sleep 20
+
+systemctl enable xray && systemctl restart xray
+#systemctl disable v2ray
 cd ~
 wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/xrayud.sh
 chmod +x xrayud.sh
 crontab -l > crontab.bak
 
 #echo "0 1 * * * apt update && apt upgrade -y" >> crontab.bak
-sed -e '/xrayud/d' crontab.bak > crontab.bak.new
-echo "0 1 * * * bash xrayud.sh" >> crontab.bak.new
-crontab crontab.bak.new
+sed -i '/v2rayud/d' crontab.bak
+sed -i '/xrayud/d' crontab.bak
+echo "0 1 * * * bash xrayud.sh" >> crontab.bak
+#echo "30 3 1 * * service caddy restart" >> crontab.bak
+crontab crontab.bak
 apt install -y expect
 wget --no-check-certificate -O install_bbr_expect.sh https://raw.githubusercontent.com/zcluo/vps/master/shell/install_bbr_expect.sh
 chmod +x install_bbr_expect.sh
 ./install_bbr_expect.sh
 
-#wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
-#chmod +x shadowsocks-all.sh
-#wget --no-check-certificate -O install_ss_expect.sh https://raw.githubusercontent.com/zcluo/vps/master/shell/install_ss_expect.sh
-#chmod +x install_ss_expect.sh
-#./install_ss_expect.sh
+
 # caddy伪装网页
 cd ~
 mkdir -p /var/www/html
@@ -82,6 +78,7 @@ wget  -N --no-check-certificate  https://raw.githubusercontent.com/zcluo/vps/mas
 unzip -o html1.zip -d /var/www/html
 
 dd if=/dev/urandom of=/var/www/html/test bs=100M count=1 iflag=fullblock
+>/etc/rc.local
 cat <<EOF >/etc/rc.local
 #!/bin/sh -e
 #
@@ -95,27 +92,34 @@ cat <<EOF >/etc/rc.local
 # bits.
 #
 # By default this script does nothing.
-
+# nohup /usr/local/bin/v2ray run -c /usr/local/etc/v2ray/config.json > /var/log/xray/nohup.log 2>&1 &
 exit 0
 EOF
 chmod +x /etc/rc.local
 systemctl start rc-local
+systemctl start nginx
 
 
-#增加trojan安装
-
-#apt install build-essential devscripts debhelper cmake libboost-system-dev libboost-program-options-dev libssl-dev default-libmysqlclient-dev python3 curl openssl -y
-#bash -c "$(curl -fsSL https://raw.githubusercontent.com/trojan-gfw/trojan-quickstart/master/trojan-quickstart.sh)"
-
-#wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/trojan.cfg -O /usr/local/etc/trojan/config.json
-#sed -e "s/xxx\.xxxxxx\.xxx/$1/g" /usr/local/etc/trojan/config.json > /usr/local/etc/trojan/config.json.new
-#\mv -f /usr/local/etc/trojan/config.json.new /usr/local/etc/trojan/config.json
-#systemctl enable trojan && systemctl restart trojan
-#cd
-#wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/snell.sh
-#chmod +x snell.sh
-#./snell.sh
 \chmod -R 777  /var/log/
 #reboot
 cd ~
-trap "rm -rf crontab* bbr.sh install-release.sh caddy_install.sh install_bbr_expect.sh all_install_xray.sh install_bbr.log html1.zip;reboot" EXIT
+#bash install_ohmyposh.sh
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+chmod +x /usr/local/bin/oh-my-posh
+
+mkdir ~/.poshthemes
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+unzip -o ~/.poshthemes/themes.zip -d  ~/.poshthemes
+chmod u+rw ~/.poshthemes/*.omp.*
+rm ~/.poshthemes/themes.zip
+echo 'eval "$(oh-my-posh --init --shell bash --config /root/.poshthemes/1_shell.omp.json)"' >> .bashrc
+
+echo "clear" >> .bashrc
+echo "neofetch" >> .bashrc
+if [ $(cat ~/.bashrc | grep neofetch |wc -l) -ge 2 ]
+then
+    a=$(sed -n "$=" ~/.bashrc )
+    let b=a-3+1
+    sed -i $(($b)),$(($a))d ~/.bashrc
+fi
+trap "rm -rf crontab* bbr.sh install-release.sh caddy_install.sh install_bbr_expect.sh all_install.sh install_bbr.log html1.zip xrayud.sh;reboot" EXIT
