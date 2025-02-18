c2ee1c0b21b9c56ea35b7843d271a84350caf8a7
zcluo <zhcluo@gmail.com>
2025-02-18 16:41:46 +0800

    Update all_install_xray.sh

--- shell/all_install_xray.sh
+++ shell/all_install_xray.sh
@@ -1,5 +1,25 @@
 #!/bin/bash
-if [ $# != 9 ] ; then
+set -e
+startTime=$(date +%Y%m%d-%H:%M)
+cleanup() {
+    local exit_status=$?
+    if [ $exit_status -eq 0 ]; then
+        echo "Start Installed at $startTime successfully!" >>~/install.log
+        rm -rf smartdns.tar.gz smartdns.sh smartdns fastfetch-linux-amd64.deb crontab* bbr.sh install-release.sh caddy_install.sh install_bbr_expect.sh all_install.sh all_install_xray.sh install_bbr.log html1.zip v2rayud.sh
+        reboot
+    else
+        echo "[DEBUG] 捕获退出信号，状态码: $exit_status"
+        echo "Start Installed at $startTime failed!" >> ~/install.log
+        rm -rf smartdns.tar.gz smartdns.sh smartdns fastfetch-linux-amd64.deb crontab* bbr.sh install-release.sh caddy_install.sh install_bbr_expect.sh all_install.sh all_install_xray.sh install_bbr.log html1.zip v2rayud.sh
+        
+
+    fi
+    exit $exit_status
+
+    
+}
+trap cleanup EXIT INT TERM
+if [ $# -ne 9 ]; then
 echo "USAGE: $0 domain_name username password emailaddress uuid realityprivkey grpc_port tcp_port xhttp_port"
 echo " e.g.: $0 domain_name username password emailaddress uuid realityprivkey  grpc_port tcp_port xhttp_port"
 exit 1;
@@ -7,7 +27,8 @@ fi
 
 systemctl stop xray && systemctl disable xray
 systemctl stop v2ray && systemctl disable v2ray
-ps -ef | grep v2ray | grep -v grep | awk {'print $2'} | xargs kill -9
+#ps -ef | grep v2ray | grep -v grep | awk '{print $2}' | xargs kill -9
+pgrep -f v2ray | awk '{print $2}' | xargs kill -9
 
 #used for uuid replacement
 #uuid=$(cat /proc/sys/kernel/random/uuid)
@@ -20,15 +41,15 @@ apt install curl screen net-tools iperf3 ca-certificates git lsof apt-transport-
 #wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/caddy_install.sh && chmod +x caddy_install.sh && bash caddy_install.sh install
 #wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/across/master/bbr.sh
 systemctl stop nginx
-#\rm -f /etc/apt/sources.list.d/caddy-fury.list
-\chmod -R 777  /var/log/
+#rm -f /etc/apt/sources.list.d/caddy-fury.list
+chmod -R 777 /var/log/
 wget -N --no-check-certificate https://raw.githubusercontent.com/teddysun/across/master/bbr.sh
 #wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/install_ohmyposh.sh
 #mkdir -p /usr/local/caddy/
-\mkdir -p /var/log/xray/
-\mkdir -p /var/log/nginx
-\chmod -R 777  /var/log/xray/
-\chmod -R 777 /var/log/nginx
+mkdir -p /var/log/xray/
+mkdir -p /var/log/nginx
+chmod -R 777 /var/log/xray/
+chmod -R 777 /var/log/nginx
 bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release.sh)" @ install  --beta
 #curl -O https://raw.githubusercontent.com/v2fly/fhs-install-v2ray/master/install-release.sh
 #chmod +x install-release.sh
@@ -36,7 +57,7 @@ bash -c "$(curl -L https://github.com/XTLS/Xray-install/raw/main/install-release
 sleep 20
 
 #\mkdir -p /etc/caddy/
-\rm -rf /usr/local/etc/xray/*
+rm -rf /usr/local/etc/xray/*
 
 
 wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/config_xray.json -O /usr/local/etc/xray/config.json
@@ -59,12 +80,12 @@ sed -i "s/port_xhttp/$9/g"   /usr/local/etc/xray/config.json
 
 
 #chmod -x /etc/systemd/system/xray.service
-\chmod -R 777 /etc/letsencrypt
+chmod -R 777 /etc/letsencrypt
 sleep 20
 
 systemctl enable xray && systemctl restart xray
 #systemctl disable v2ray
-cd ~
+cd ~ || exit
 wget -N --no-check-certificate https://raw.githubusercontent.com/zcluo/vps/master/shell/xrayud.sh
 chmod +x xrayud.sh
 crontab -l > crontab.bak
@@ -86,19 +107,19 @@ chmod +x install_bbr_expect.sh
 #chmod +x mosdns.sh
 #bash mosdns.sh $1
 
-cd ~
+cd ~ || exit
 wget --no-check-certificate -O smartdns.sh https://raw.githubusercontent.com/zcluo/vps/master/shell/smartdns.sh
 chmod +x smartdns.sh
 bash smartdns.sh $1
 
 # caddy伪装网页
-cd ~
+cd ~ || exit
 mkdir -p /var/www/html
 wget  -N --no-check-certificate  https://raw.githubusercontent.com/zcluo/vps/master/shell/html1.zip
 unzip -o html1.zip -d /var/www/html
 
 dd if=/dev/urandom of=/var/www/html/test bs=100M count=1 iflag=fullblock
->/etc/rc.local
+echo "" >/etc/rc.local
 cat <<EOF >/etc/rc.local
 #!/bin/sh -e
 #
@@ -120,9 +141,9 @@ systemctl start rc-local
 systemctl start nginx
 
 
-\chmod -R 777  /var/log/
+chmod -R 777 /var/log/
 #reboot
-cd ~
+cd ~ || exit
 #bash install_ohmyposh.sh
 wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
 chmod +x /usr/local/bin/oh-my-posh
@@ -132,14 +153,23 @@ wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/theme
 unzip -o ~/themes/themes.zip -d  ~/themes
 chmod u+rw ~/themes/*.omp.*
 rm ~/themes/themes.zip
+echo '[ -z "$PS1" ] && return' >> .bashrc
 echo 'eval "$(oh-my-posh --init --shell bash --config /root/themes/1_shell.omp.json)"' >> .bashrc
-
 echo "clear" >> .bashrc
 echo "fastfetch" >> .bashrc
-if [ $(cat ~/.bashrc | grep fastfetch |wc -l) -ge 2 ]
-then
-    a=$(sed -n "$=" ~/.bashrc )
-    let b=a-3+1
-    sed -i $(($b)),$(($a))d ~/.bashrc
+# 计算包含 "fastfetch" 的行数
+fastfetch_count=$(grep -c "fastfetch" ~/.bashrc)
+
+# 判断是否有多余的 "fastfetch" 行
+if [ "$fastfetch_count" -ge 2 ]; then
+    # 获取文件总行数
+    total_lines=$(wc -l < ~/.bashrc)
+    
+    # 计算需要删除的行范围
+    start_line=$((total_lines - 4 + 1))
+    end_line=$total_lines
+    
+    # 删除多余的行
+    sed -i "${start_line},${end_line}d" ~/.bashrc
 fi
-trap "rm -rf smartdns.tar.gz smartdns.sh smartdns fastfetch-linux-amd64.deb crontab* bbr.sh install-release.sh caddy_install.sh install_bbr_expect.sh all_install.sh all_install_xray.sh install_bbr.log html1.zip v2rayud.sh;reboot" EXIT
+
